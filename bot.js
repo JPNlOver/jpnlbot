9f8b90f61fa211deccb1b8acdc9805e6067ab559
JPNlOver <40501735+JPNlOver@users.noreply.github.com>
2018-06-23 11:53:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const botSettings = require("/botSettings.json");
+const botSettings = require("jpnlbot/app/botSettings.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({autoReconnect:true});
